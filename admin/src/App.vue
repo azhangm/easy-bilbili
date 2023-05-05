1ae6867635e883b87de4ede8158289712d5bf379
da juancai[2~ <742824624@qq.com>
2023-05-05 19:15:53 +0800

    :white_check_mark:简化登录页面，后台管理用户一般由管理员添加，不允许注册和使用第三方登录。

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -7,21 +7,18 @@
             <div class="center">
               <h1>
                 <i class="ace-icon fa fa-leaf green"></i>
-                <span class="red">Ace</span>
-                <span class="white" id="id-text2">Application</span>
+                <span class="dark" id="id-text2">后台管理系统</span>
               </h1>
-              <h4 class="blue" id="id-company-text">&copy; Company Name</h4>
             </div>
 
-            <div class="space-6"></div>
 
             <div class="position-relative">
               <div id="login-box" class="login-box visible widget-box no-border">
                 <div class="widget-body">
                   <div class="widget-main">
-                    <h4 class="header blue lighter bigger">
-                      <i class="ace-icon fa fa-coffee green"></i>
-                      Please Enter Your Information
+                    <h4 class="header blue lighter bigger center">
+                      <i class="ace-icon fa fa-coffee green "></i>
+                      登录
                     </h4>
 
                     <div class="space-6"></div>
@@ -30,14 +27,14 @@
                       <fieldset>
                         <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username" />
+															<input type="text" class="form-control" placeholder="账号" />
 															<i class="ace-icon fa fa-user"></i>
 														</span>
                         </label>
 
                         <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password" />
+															<input type="password" class="form-control" placeholder="密码" />
 															<i class="ace-icon fa fa-lock"></i>
 														</span>
                         </label>
@@ -47,59 +44,21 @@
                         <div class="clearfix">
                           <label class="inline">
                             <input type="checkbox" class="ace" />
-                            <span class="lbl"> Remember Me</span>
+                            <span class="lbl">&nbsp;记住我</span>
                           </label>
 
                           <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
                             <i class="ace-icon fa fa-key"></i>
-                            <span class="bigger-110">Login</span>
+                            <span class="bigger-110">登录</span>
                           </button>
                         </div>
 
                         <div class="space-4"></div>
                       </fieldset>
                     </form>
-
-                    <div class="social-or-login center">
-                      <span class="bigger-110">Or Login Using</span>
-                    </div>
-
-                    <div class="space-6"></div>
-
-                    <div class="social-login center">
-                      <a class="btn btn-primary">
-                        <i class="ace-icon fa fa-facebook"></i>
-                      </a>
-
-                      <a class="btn btn-info">
-                        <i class="ace-icon fa fa-twitter"></i>
-                      </a>
-
-                      <a class="btn btn-danger">
-                        <i class="ace-icon fa fa-google-plus"></i>
-                      </a>
-                    </div>
                   </div><!-- /.widget-main -->
-
-                  <div class="toolbar clearfix">
-                    <div>
-                      <a href="#" data-target="#forgot-box" class="forgot-password-link">
-                        <i class="ace-icon fa fa-arrow-left"></i>
-                        I forgot my password
-                      </a>
-                    </div>
-
-                    <div>
-                      <a href="#" data-target="#signup-box" class="user-signup-link">
-                        I want to register
-                        <i class="ace-icon fa fa-arrow-right"></i>
-                      </a>
-                    </div>
-                  </div>
                 </div><!-- /.widget-body -->
               </div><!-- /.login-box -->
-
-
             </div><!-- /.position-relative -->
           </div>
         </div><!-- /.col -->
