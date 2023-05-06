26e2d6073aca4a1d4266dae207104bc7ab1be5c3
da juancai[2~ <742824624@qq.com>
2023-05-06 09:55:12 +0800

    :white_check_mark:添加新模块 business

---
+++ business/src/main/java/nuc/zm/business/BusinessApplication.java
@@ -0,0 +1,24 @@
+package nuc.zm.business;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("nuc.zm.business.mapper")
+public class BusinessApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(BusinessApplication.class, args);
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("business 地址 : \t http://127.0.0.1:{}",environment.getProperty("server.port"));
+
+    }
+}
