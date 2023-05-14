a4d8657846f6eae6b2b41a7d0ffa9c3daaaf1a27
da juancai[2~ <742824624@qq.com>
2023-05-14 10:03:52 +0800

    :white_check_mark: 信息摘要算法MD5工具类 随机盐值

---
+++ server/src/main/java/nuc/zm/server/util/MD5Util.java
@@ -0,0 +1,22 @@
+package nuc.zm.server.util;
+
+import cn.hutool.crypto.digest.DigestAlgorithm;
+import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.crypto.digest.Digester;
+
+/**
+ * md5 信息摘要算法加密工具类
+ *
+ * @author zm
+ * @date 2023/05/14
+ */
+public class MD5Util {
+
+    public static String sign(String text,String salt,String charset) {
+        return DigestUtil.md5Hex(text + salt, charset);
+    }
+
+    public static boolean verity(String text,String salt,String sign,String charset){
+        return DigestUtil.md5Hex(text + salt, charset).equals(sign);
+    }
+}
